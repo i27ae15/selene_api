da043557e10726450e59c428084271ec333cb392
Andres Melendes <andresruse18@gmail.com>
2022-09-04 22:02:12 -0400

    create models

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+from django.utils import timezone
+
+from selene_models.models import SeleneModel, SeleneBot, Context
+
+class Interaction(models.Model):
+
+    selene_bot = models.ForeignKey(SeleneBot, on_delete=models.CASCADE)
+
+    started_at = models.DateTimeField()
+    finished_at = models.DateTimeField()
+    is_client_satified = models.BooleanField()
+
+
+class MessageSent(models.Model):
+
+    # Foreign keys -----------------------------------------
+    context = models.ForeignKey(Context, on_delete=models.CASCADE)
+
+    interaction = models.ForeignKey(Interaction, on_delete=models.CASCADE)
+
+
+    # ------------------------------------------------------
+
+    message = models.TextField()
+
+    sender = models.CharField(max_length=255)
+    sent_at = models.DateTimeField()
+
+    understood_within_context = models.BooleanField()
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.sent_at = timezone.now()
+        super().save(*args, **kwargs)
--- selene_models/models.py
+++ selene_models/models.py
@@ -1,2 +1,69 @@
+import secrets
+
 from django.db import models
+
+from django.utils import timezone
+
+class SeleneModel(models.Model):
+
+    created_at = models.DateTimeField()
+
+    model = models.FileField(upload_to='models')
+
+    name = models.CharField(max_length=255)
+
+    updated_at = models.DateTimeField()
+
+    data_trained_with = models.JSONField() # should this be a file field?
+
+    @property
+    def active_bots(self):
+        return self.selenebot_set.filter(is_active=True)
+
+    def __str__(self):
+        return self.name
+
+
+class SeleneBot(models.Model):
+
+    # Foreign keys -----------------------------------------
+    model = models.ForeignKey(SeleneModel, on_delete=models.CASCADE)
+    # ------------------------------------------------------
+
+    active = models.BooleanField(default=True)
+
+    created_at = models.DateTimeField()
+
+    token = models.CharField(max_length=255)
+
+    @property
+    def number_of_interactions(self) -> int:
+        return self.interactions_set.all().count()
+    
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.token = secrets.token_hex(16)
+            self.created_at = timezone.now()
+        super().save(*args, **kwargs)
+
+
+class Context(models.Model):
+    # Foreign keys -----------------------------------------
+    model = models.ForeignKey(SeleneModel, on_delete=models.CASCADE)
+    # ------------------------------------------------------
+
+    data_trained_with = models.JSONField() # should this be a file field?
+
+    name = models.CharField(max_length=255)
+
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.created_at = timezone.now()
+        else:
+            self.updated_at = timezone.now()
+        super().save(*args, **kwargs)
+
 
